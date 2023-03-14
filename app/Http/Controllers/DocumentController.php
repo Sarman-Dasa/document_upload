f696dd3fd9eb64ffd6d38055e2e7698d94dd50e9
Sarman-Dasa <sarmand@zignuts.com>
2023-03-14 19:26:49 +0530

    crud operation for document

---
+++ app/Http/Controllers/DocumentController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Document;
+use Illuminate\Http\Request;
+
+class DocumentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $documentList = Document::all();
+        return view('documentList.documentIndex',compact('documentList'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('documentList.adddocument');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'  =>  'required|min:5|max:50|unique:documents,name',
+        ]);
+
+        $document = Document::create($request->only('name'));
+        return redirect()->route('success.msg')->with('success','Document Add Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $document = Document::findOrFail($id);
+        return view('documentList.adddocument',compact('document'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+       $document = Document::findOrFail($id);
+       $document->delete();
+       return redirect()->route('success.msg')->with('success','document deleted');
+    }
+}
