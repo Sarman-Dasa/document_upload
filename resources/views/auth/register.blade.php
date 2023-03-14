59e5622b60fe48ba39ec1898b6c9c8bd4d7fcc01
Sarman-Dasa <sarmand@zignuts.com>
2023-03-14 19:30:39 +0530

    registration  design code

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,56 @@
+@include('layout.link')
+<div class="contenier table table-secondary mt-5 mb-5">
+    <div class="row">
+        <div class="col-md-4 offset-md-4">
+            <div class="md-4 text-center bg-secondary">
+                <h3>User Registration</h3>
+            </div>
+            <form action="{{ route('register') }}" method="POST" class="p-4">
+                @csrf
+                <div class="form-group mb-3">
+                    <label for="">Name</label>
+                    <input type="text" name="name" class="form-control" value="{{old('name')}}">
+                    <span class="text-danger">
+                        @error('name')
+                            {{ $message }}
+                        @enderror
+                    </span>
+                </div>
+
+                <div class="form-group mb-3">
+                    <label for="">Email</label>
+                    <input type="email" name="email" class="form-control"  value="{{old('email')}}">
+                    <span class="text-danger">
+                        @error('email')
+                            {{ $message }}
+                        @enderror
+                    </span>
+                </div>
+
+                <div class="form-group mb-3">
+                    <label for="">Password</label>
+                    <input type="password" name="password" class="form-control">
+                    <span class="text-danger">
+                        @error('password')
+                            {{ $message }}
+                        @enderror
+                    </span>
+                </div>
+
+                <div class="form-group mb-3">
+                    <label for="">Confirm Password</label>
+                    <input type="password" name="password_confirmation" class="form-control">
+                    <span class="text-danger">
+                        @error('password_confirmation')
+                            {{ $message }}
+                        @enderror
+                    </span>
+                </div>
+
+                <div class="form-group d-grid">
+                    <input type="submit" value="Register" class="btn btn-info bg-secondary-subtle b-block">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
