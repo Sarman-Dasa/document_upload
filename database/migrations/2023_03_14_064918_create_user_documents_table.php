76e40df09680e3b58d65b9b6da2a78828c2d4c1d
Sarman-Dasa <sarmand@zignuts.com>
2023-03-14 19:29:29 +0530

    create user_documents table

---
+++ database/migrations/2023_03_14_064918_create_user_documents_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('path',200);
+            $table->timestamp('expired_date');
+            $table->boolean('status')->default(true);
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('document_id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('document_id')->references('id')->on('documents')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_documents');
+    }
+};
