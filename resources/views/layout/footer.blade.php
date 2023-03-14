84e309fbeea858f685e8bc6f38b08e8e5ae7714a
Sarman-Dasa <sarmand@zignuts.com>
2023-03-14 19:32:19 +0530

    add footer file

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="card text-center">
+    <div class="card-body">
+      <h5 class="card-title">Document</h5>
+     
+    </div>
+    <div class="card-footer text-muted">
+      @copyrigth
+    </div>
+</div>
